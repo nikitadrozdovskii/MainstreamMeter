2328b681087e260d050d09a9ff310dec00bef313
nikitadrozdovskii <butch386@gmail.com>
2018-06-08 08:27:51 -0500

    moved getting album id and album object functionality from app.js to api.js

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 class Api{
     constructor(){
+        this.token='';
     }
 
     //extract token from URL
@@ -7,8 +8,33 @@ class Api{
         var search = location.hash.substring(1);
         var urlHash = search?JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g,'":"') + '"}',
             function(key, value) { return key===""?value:decodeURIComponent(value) }):{}
-        let token = urlHash.access_token;
-        return token;
+        this.token = urlHash.access_token;
+
+    }
+
+    async getAlbumObject(artist,album){
+        //make fetch request to get album's id
+        const response = await fetch(`https://api.spotify.com/v1/search?q=album%3A${album}%20artist%3A${artist}&type=album`,
+            {method: 'GET',
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${this.token}`
+                }});
+
+        const responseData = await response.json();
+        let album_id = responseData.albums.items[0].id;
+
+        //make fetch request to get album's detailed object
+        const response2 = await fetch(`https://api.spotify.com/v1/albums/${album_id}?market=ES`,
+            {method: 'GET',
+                headers: {
+                    "Authorization": `Bearer ${this.token}`,
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                }});
+        const responseData2 = await response2.json();
+        return responseData2;
     }
 
 
--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const albumSearch = document.getElementById('albumSearch');
 const artistSearch = document.getElementById('artistSearch');
 
 const api = new Api();
-let token = api.parseURLHash();
-console.log(`token from api class: ${token}`);
+api.parseURLHash();
 
 getAlbum.addEventListener('click',displayAlbum);
 
@@ -15,37 +14,42 @@ getAlbum.addEventListener('click',displayAlbum);
 
 async function displayAlbum(e){
     e.preventDefault();
-    console.log(albumSearch.value);
+    //console.log(albumSearch.value);
     //Make fetch GET request to search for album
-    console.log(`https://api.spotify.com/v1/search?q=album%3A${albumSearch.value}%20artist%3A${artistSearch.value}&type=album`);
-    const response = await fetch(`https://api.spotify.com/v1/search?q=album%3A${albumSearch.value}%20artist%3A${artistSearch.value}&type=album`,
-        {method: 'GET',
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
-            }});
+    // const response = await fetch(`https://api.spotify.com/v1/search?q=album%3A${albumSearch.value}%20artist%3A${artistSearch.value}&type=album`,
+    //     {method: 'GET',
+    //         headers: {
+    //             "Accept": "application/json",
+    //             "Content-Type": "application/json",
+    //             "Authorization": `Bearer ${token}`
+    //         }});
+    //
+    // const responseData = await response.json();
+    // console.log(responseData);
+    // let album_id = responseData.albums.items[0].id;
+    //
+    // //make fetch request to get album's popularity
+    // const response2 = await fetch(`https://api.spotify.com/v1/albums/${album_id}?market=ES`,
+    //     {method: 'GET',
+    //         headers: {
+    //             "Authorization": `Bearer ${token}`,
+    //             "Accept": "application/json",
+    //             "Content-Type": "application/json"
+    //         }});
+    // const responseData2 = await response2.json();
+    // console.log(responseData2);
+    let album;
+    api.getAlbumObject(artistSearch.value,albumSearch.value).then((alb)=>{
+        console.log(alb);
+        albumDiv.innerHTML = `
+        <img src="${alb.images[1].url}" alt="">
+        <h2>artist: ${alb.artists[0].name}</h2>
+        <h2>name: ${alb.name}</h2>
+        <h2>popularity: ${alb.popularity}</h2>`;
+    });
 
-    const responseData = await response.json();
-    console.log(responseData);
-    let album_id = responseData.albums.items[0].id;
-
-    //make fetch request to get album's popularity
-    const response2 = await fetch(`https://api.spotify.com/v1/albums/${album_id}?market=ES`,
-        {method: 'GET',
-            headers: {
-                "Authorization": `Bearer ${token}`,
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            }});
-    const responseData2 = await response2.json();
-    console.log(responseData2);
 
 
-    albumDiv.innerHTML = `
-    <img src="${responseData2.images[1].url}" alt="">
-    <h2>artist: ${responseData2.artists[0].name}</h2>
-    <h2>name: ${responseData2.name}</h2> 
-    <h2>popularity: ${responseData2.popularity}</h2>`;
-    console.log(responseData2);
+
+    // console.log(responseData2);
 }
